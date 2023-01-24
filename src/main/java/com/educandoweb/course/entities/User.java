bf81b462467ededa2a5c54ea4d7008d7ea12f54b
Juvencio Linhares <juvlinhares@gmail.com>
2023-01-24 00:40:43 -0300

    h2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -3,13 +3,20 @@ package com.educandoweb.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
-import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-
+@Entity //pra dizer ao d que é uma entidade
+@Table(name = "tb_user") // dar o nome da tabela
 public class User implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id //diz ao bd que o id é o pk
+	@GeneratedValue(strategy = GenerationType.IDENTITY)//pra dizer que é auto incrementada
 	private Long id;
 	private String name;
 	private String email;
